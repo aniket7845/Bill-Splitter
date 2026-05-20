5e6bed1a8deb72a69f9d736f00fa7265fb9c49b9
aniket7845 <127074898+aniket7845@users.noreply.github.com>
2026-05-20 19:07:46 +0530

    changes

---
+++ android/app/src/main/java/com/aniket/billsplitter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aniket.billsplitter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+   appId: 'com.aniket.billsplitter',
+  appName: 'Bill Splitter',
+  webDir: 'dist/bill-splitter/browser',
+  bundledWebRuntime: false
+};
+
+export default config;
